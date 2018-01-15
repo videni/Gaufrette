a0dae87b0be89b097e7e540eca2623fc5b892717
Nicolas MURE <nicolas.mure@knplabs.com>
2018-01-15 21:54:31 +0100

    update opencloud functional tests

--- doc/adapters/open-cloud.md
+++ doc/adapters/open-cloud.md
@@ -12,20 +12,26 @@ composer require gaufrette/opencloud-adapter
 To use the OpenCloud adapter you will need to create a connection using the
 [OpenCloud SDK](https://github.com/php-opencloud/openstack).
 
+## Usage with Identity API v3
+
+For services using the [OpenStack Identity API v3](https://developer.openstack.org/api-ref/identity/v3/index.html) :
+
 ```php
 use Gaufrette\Adapter\OpenCloud as OpenCloudAdapter;
 use Gaufrette\Filesystem;
 use OpenStack\OpenStack;
 
-$openstack = new OpenStack([
-    'username' => 'your username',
-    'password' => 'your Keystone password',
-    'tenantName' => 'your tenant (project) name',
-    'authUrl' => 'https://example.com/v2/identity',
-]);
+$objectStore = (new OpenStack([
+        'username' => 'your username',
+        'password' => 'your Keystone password',
+        'tenantName' => 'your tenant (project) name',
+        'authUrl' => 'https://example.com/v3/identity',
+    ]))
+    ->objectStoreV1()
+;
 
 $adapter = new OpenCloudAdapter(
-    $openstack->objectStoreV1(),
+    $objectStore,
     'container-name',
     true // optional, indicates whether to create the container or not
          // if it does not exist, default to false
@@ -36,3 +42,46 @@ $filesystem = new Filesystem($adapter);
 
 See [here](https://github.com/php-opencloud/openstack/blob/master/src/OpenStack.php)
 for all OpenStack connection options.
+
+## Usage with Identity API v2
+
+For services using the [OpenStack Identity API v2](https://developer.openstack.org/api-ref/identity/v2/),
+such as [rackspace.com](https://www.rackspace.com/) :
+
+```php
+use Gaufrette\Adapter\OpenCloud as OpenCloudAdapter;
+use Gaufrette\Filesystem;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use OpenStack\Identity\v2\Service as IdentityService;
+use OpenStack\OpenStack;
+
+$objectStore = new (OpenStack([
+        'username' => 'your username',
+        'password' => 'your password',
+        'tenantId' => 'your tenant Id (also known as account Id/number)'
+        'authUrl' => 'https://example.com/v2/identity',
+        'region' => 'the cloud region (eg "LON" for London)',
+        'identityService' => IdentityService::factory(
+            new Client([
+                'base_uri' => 'https://example.com/v2/identity',
+                'handler' => HandlerStack::create(),
+            ])
+        ),
+    ]))
+    ->objectStoreV1([
+        'catalogName' => 'cloudFiles', // default to "swift", use "cloudFiles" for rackspace,
+                                       // or find the catalog name of your cloud service
+                                       // associated with the "object-store" catalog type
+    ])
+;
+
+$adapter = new OpenCloudAdapter(
+    $objectStore,
+    'container-name',
+    true // optional, indicates whether to create the container or not
+         // if it does not exist, default to false
+);
+
+$filesystem = new Filesystem($adapter);
+```
--- tests/Gaufrette/Functional/Adapter/OpenCloudTest.php
+++ tests/Gaufrette/Functional/Adapter/OpenCloudTest.php
@@ -4,11 +4,14 @@ namespace Gaufrette\Functional\Adapter;
 
 use Gaufrette\Adapter\OpenCloud;
 use Gaufrette\Filesystem;
-use OpenCloud\Rackspace;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use OpenStack\Identity\v2\Service as IdentityService;
+use OpenStack\OpenStack;
 
 class OpenCloudTest extends FunctionalTestCase
 {
-    /** @var \OpenCloud\ObjectStore\Service */
+    /** @var \OpenStack\ObjectStore\v1\Service */
     private $objectStore;
 
     /** @var string */
@@ -16,23 +19,42 @@ class OpenCloudTest extends FunctionalTestCase
 
     public function setUp()
     {
-        $username = getenv('RACKSPACE_USER') ?: '';
-        $apiKey = getenv('RACKSPACE_APIKEY') ?: '';
-        $container = getenv('RACKSPACE_CONTAINER') ?: '';
+        $username = getenv('RACKSPACE_USERNAME') ?: '';
+        $password = getenv('RACKSPACE_PASSWORD') ?: '';
+        $tenantId = getenv('RACKSPACE_TENANT_ID') ?: '';
+        $region = getenv('RACKSPACE_REGION') ?: '';
 
-        if (empty($username) || empty($apiKey) || empty($container)) {
-            $this->markTestSkipped('Either RACKSPACE_USER, RACKSPACE_APIKEY and/or RACKSPACE_CONTAINER env vars are missing.');
+        if (empty($username) || empty($password) || empty($tenantId) || empty($region)) {
+            $this->markTestSkipped('Either RACKSPACE_USERNAME, RACKSPACE_PASSWORD, RACKSPACE_TENANT_ID and/or RACKSPACE_REGION env vars are missing.');
         }
 
-        $connection = new Rackspace('https://identity.api.rackspacecloud.com/v2.0/', [
-            'username' => $username,
-            'apiKey' => $apiKey,
+        $authUrl = 'https://identity.api.rackspacecloud.com/v2.0/';
+
+        /*
+         * Rackspace uses OpenStack Identity v2
+         * @see https://github.com/php-opencloud/openstack/issues/127
+         */
+        $this->container = uniqid('gaufretteci');
+        $this->objectStore = (new OpenStack([
+                'username' => $username,
+                'password' => $password,
+                'tenantId' => $tenantId,
+                'authUrl' => $authUrl,
+                'region' => $region,
+                'identityService' => IdentityService::factory(
+                    new Client([
+                        'base_uri' => $authUrl,
+                        'handler' => HandlerStack::create(),
+                    ])
+                ),
+            ]))
+            ->objectStoreV1([
+                'catalogName' => 'cloudFiles',
+            ]);
+
+        $this->objectStore->createContainer([
+            'name' => $this->container,
         ]);
-
-        $this->container = uniqid($container);
-        $this->objectStore = $connection->objectStoreService('cloudFiles', 'IAD', 'publicURL');
-        $this->objectStore->createContainer($this->container);
-
         $adapter = new OpenCloud($this->objectStore, $this->container);
         $this->filesystem = new Filesystem($adapter);
     }
@@ -43,6 +65,81 @@ class OpenCloudTest extends FunctionalTestCase
             return;
         }
 
-        $this->objectStore->getContainer($this->container)->delete(true);
+        // rackspace container must be empty to be deleted
+        array_map(function ($key) {
+            $this->filesystem->delete($key);
+        }, $this->filesystem->keys());
+
+        $this->objectStore->getContainer($this->container)->delete();
+    }
+
+    /**
+     * @test
+     * @group functional
+     */
+    public function shouldGetChecksum()
+    {
+        $this->filesystem->write('foo', 'Some content');
+
+        $this->assertEquals(md5('Some content'), $this->filesystem->checksum('foo'));
+    }
+
+    /**
+     * @test
+     * @group functional
+     */
+    public function shouldGetSize()
+    {
+        $this->filesystem->write('foo', 'Some content');
+
+        $this->assertEquals(strlen('Some content'), $this->filesystem->size('foo'));
+    }
+
+    /**
+     * @test
+     * @group functional
+     */
+    public function shouldGetMimeType()
+    {
+        $this->filesystem->write('foo.txt', 'Some content');
+
+        $this->assertEquals('text/plain', $this->filesystem->mimeType('foo.txt'));
+    }
+
+    /**
+     * @test
+     * @group functional
+     */
+    public function shouldSetAndGetMetadata()
+    {
+        $this->filesystem->write('test.txt', 'Some content');
+        $this->filesystem->getAdapter()->setMetadata('test.txt', [
+            'Some-Meta' => 'foo',
+            'Custom-Stuff' => 'bar',
+        ]);
+
+        $this->assertEquals([
+            'Some-Meta' => 'foo',
+            'Custom-Stuff' => 'bar',
+        ], $this->filesystem->getAdapter()->getMetadata('test.txt'));
+    }
+
+    /*
+     * OVERWRITE PARENT FUNCTIONS
+     * @TODO : REMOVE THE BELOW OVERWRITES WHEN
+     *         https://github.com/KnpLabs/Gaufrette/pull/521 WILL BE MERGED.
+     */
+
+    /**
+     * @test
+     * @group functional
+     */
+    public function shouldWriteAndRead()
+    {
+        $this->filesystem->write('foo', 'Some content');
+        $this->filesystem->write('test/subdir/foo', 'Some content1', true);
+
+        $this->assertEquals('Some content', $this->filesystem->read('foo'));
+        $this->assertEquals('Some content1', $this->filesystem->read('test/subdir/foo'));
     }
 }
